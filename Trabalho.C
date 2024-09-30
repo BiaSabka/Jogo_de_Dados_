201e30c7ce50486af6298e151b7ceebf68dcd190
Bianca Sabka de Queiroz <biancasabka@gmail.com>
2024-09-29 21:23:51 -0400

    UpdateTrabalho

---
+++ Trabalho.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <Stdlib.h>
+int main()
+{
+  float ValAposta;
+  system("cls");
+
+  printf("Escolha o Valor que Gostaria de Apostar: ");
+  scanf("%f", &ValAposta);
+  system("cls");
+
+  printf("O Valor Apostado foi %.2f"), ValAposta;
+}
